c03e44d4d55442e6e1264a4826796338edf8ad85
Rupesh <rupesh.mayekar@bookmyshow.com>
2018-12-12 11:39:55 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./app/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader" // compiles Less to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./app/index.html",
+      hash: true,
+      filename: "index.html"
+    })
+  ]
+};
